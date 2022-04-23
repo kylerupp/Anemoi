217ca9a6af0f6c400c712d8b778251f0a97e090a
Kyle Rupp <kyle.rupp838@gmail.com>
2022-04-22 18:42:38 -0700

    Get current date and time in json format

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, World!"
+
+@app.route("/config", methods =['GET'])
+def get_config():
+    time = datetime.now()
+    return jsonify({'date': time})
