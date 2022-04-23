60c81b00ddf6cd3aad52b94807b9eeaec926b6ab
Kyle Rupp <kyle.rupp838@gmail.com>
2022-04-22 19:37:18 -0700

    Baseline post message for data

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
 app = Flask(__name__)
 
@@ -10,3 +10,11 @@ def home():
 def get_config():
     time = datetime.now()
     return jsonify({'date': time})
+
+@app.route('/data', methods =['POST'])
+def handle_data():
+    endpoint_params = request.json
+    print(endpoint_params['id'])
+    print(endpoint_params['temp'])
+    print(endpoint_params['time'])
+    return jsonify({'message': 'OK'})
