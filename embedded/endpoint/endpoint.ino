b78235a669a88e2c828375c5365818a778a97feb
Kyle Rupp <kyle.rupp838@gmail.com>
2022-04-28 22:12:19 -0700

    [AN-12] Keep track of firmware on endpoint

--- embedded/endpoint/endpoint.ino
+++ embedded/endpoint/endpoint.ino
@@ -2,51 +2,56 @@
 #include <ESP8266WiFi.h>
 #include <CommonDef.h>
 
+#define FIRMWARE "0.0.1"
+
 HTTPClient http;
 WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
   delay(10);
-  Serial.println('\n');
+  //Serial.println('\n');
 
   WiFi.begin(SSID_NET, SSID_KEY);
 
-  Serial.println("Connecting ...");
+  //Serial.println("Connecting ...");
 
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println('.');
+    //Serial.println('.');
   }
-  Serial.println('\n');
-  Serial.println("Connected to ");
-  Serial.println(WiFi.SSID());
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());
+  //Serial.println('\n');
+  //Serial.println("Connected to ");
+  //Serial.println(WiFi.SSID());
+  //Serial.print("IP address:\t");
+  //println(WiFi.localIP());
 
   String mac = WiFi.macAddress();
   mac.replace(':', '_');
 
   http.begin(client, "http://" SERVER "/endpoint/" + mac);
   http.addHeader("Content-Type", "application/json");
-  Serial.println("Looking for endpoint " + mac);
+  //Serial.println("Looking for endpoint " + mac);
   String payload;
   int httpCode = http.GET();
   if(httpCode == 404) {
-    Serial.println("Couldn't find my endpoint... creating one.");
+    //Serial.println("Couldn't find my endpoint... creating one.");
     http.POST("{\"mac\":\"" + mac + "\"}");
-    payload = http.getString();
-
-    Serial.println(httpCode);
-    Serial.println(payload);
+    //payload = http.getString();
+    http.end();
+    http.begin(client, "http://" SERVER "/endpoint/" + mac + "/firmware");
+    http.addHeader("Content-Type", "application/json");
+    http.POST("{\"firmware\":\"" FIRMWARE "\"}");
+    //Serial.println(httpCode);
+    //Serial.println(payload);
 
   } else {
-    Serial.println("Found my endpoint. Updating.");
+    //Serial.println("Found my endpoint. Updating.");
     http.PATCH("");
-    payload = http.getString();
+    //payload = http.getString();
 
-    Serial.println(httpCode);
-    Serial.println(payload);
+    //Serial.println(httpCode);
+    //Serial.println(payload);
     
   }
 
@@ -54,10 +59,10 @@ void setup() {
 
   http.begin(client, "http://" SERVER "/temp");
   http.addHeader("Content-Type", "application/json");
-  Serial.println("Sending fake temperature data.");
+  //Serial.println("Sending fake temperature data.");
   http.POST("{\"mac\":\"" + mac + "\",\"temp\":\"0\"}");
   http.end();
-  Serial.println("Going to sleep for 15 seconds");
+  //Serial.println("Going to sleep for 15 seconds");
   // Make sure D0 is connected to RST for reboot
   ESP.deepSleep(5e6);
 }
